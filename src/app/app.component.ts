a5e74b5c3e51b83e5c42c6b5305ceba623e6b03e
Bijin Kurien <bijin.kurien@pearson.com>
2024-08-13 13:43:46 +0530

    Initial Commit Dating AppClient Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { NgFor, CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  /*
+   * constructor injection is the typically used method for dependancy injection
+   * constructor(private http : HttpClient){}
+  */
+  
+  //below is function based injection.
+  http = inject(HttpClient);
+
   title = 'DatingAppClient';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => {
+        this.users = response;
+        console.log(this.users);
+      },
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
+  }
+
 }
